fc9be2eb6de3537553b103fd762bdb7e08e41805
Pupa-Sys <sheltonc4@live.com>
2026-01-11 23:34:07 +0200

    fixing chromium

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
     serverComponentsExternalPackages: ['@sparticuz/chromium'],
-  },
 };
 
 export default nextConfig;
