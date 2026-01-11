7a3480dcca6253de279ae06770efd4b08db783dc
Pupa-Sys <sheltonc4@live.com>
2026-01-11 23:40:22 +0200

    newly

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverComponentsExternalPackages: ['@sparticuz/chromium'],
+    serverExternalPackages: ['@sparticuz/chromium'],
 };
 
 export default nextConfig;
